011385abe458950df174ad9412327981b56df5f0
Michael Lozano <michael.lozano.mail@gmail.com>
2026-03-21 10:34:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2302954237-d6881",
+  "appId": "1:422036379862:web:61e35ff70bd0501c9d9177",
+  "apiKey": "AIzaSyAogCfhfwjhcrfoZwjXAhQKXkT-uBIpZ6Q",
+  "authDomain": "studio-2302954237-d6881.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "422036379862"
+};
